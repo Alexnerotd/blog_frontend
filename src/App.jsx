a076db4b0fdfddf1f766b775415f7abd4412d177
Alex nero <jesalemalo@gmail.com>
2024-05-12 09:49:16 -0400

    add/config router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 function App() {
-  return <>App</>;
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <p>ElementLayout</p>,
+    },
+  ]);
+  return <RouterProvider router={router} />;
 }
 
 export default App;
